91e752f134ea8fe717ff9c33f136490f7a82ed87
jptns <j.pattanasintara@gmail.com>
2026-08-01 10:05:41 +0700

    chore: remove devtools from production

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { createRootRoute, Outlet } from '@tanstack/react-router' 
-import { TanStackRouterDevtools } from '@tanstack/router-devtools' 
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { queryClient } from '../lib/queryClient'
 import { Toaster } from '@/components/ui/sonner'
 import { Header } from '@/components/common/Header' 
@@ -23,8 +22,7 @@ export const Route = createRootRoute({
             </main>
 
             <Toaster />
-            <ReactQueryDevtools buttonPosition="bottom-left" />
-            <TanStackRouterDevtools position="bottom-right" />
+
           </div>
         </FavoritesProvider>
       </CartProvider>
